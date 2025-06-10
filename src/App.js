ad022350218111cc6a5fcf92ca0778a35c9f397d
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-06-10 21:03:37 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div></div>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
+    </Router>
   );
 }
 
 export default App;
+ 
